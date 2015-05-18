5bc92361d1d495dad20bdd60eedc178ccdae1e5b
Evan Butler <evanb2@mac.com>
2015-05-17 22:33:48 -0700

    Edit readme

---
+++ README.md
@@ -0,0 +1,10 @@
+# Input Validation
+## by Evan Butler
+
+#### Setup instructions
+1. Clone this git repository
+2. Open validate.html in web browser
+
+#### Copyright © 2015, Evan Butler
+
+#### License: [MIT](https://github.com/twbs/bootstrap/blob/master/LICENSE)  
--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,8 @@
 var validateEmail = function(input) {
-    var validarray = input.search("@");
-    if (validarray === -1) {
+    var $email = $('form input[name="email');
+    var re = /[A-Z0-9._%+-]+@[A-Z0-9.-]+.[A-Z]{2,4}/igm;
+    if ($email.val() == '' || !re.test($email.val())) {
+        alert('Please enter a valid email address.');
         return "Fail";
     } else {
         return "Valid";
